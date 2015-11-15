770eaa7ab667529ab6dd6f02c06244101c29d73d
blacklight2000 <vphuvan@yahoo.com>
2015-11-15 12:52:16 -0500

    first commit on branch test-01

--- lib_mod.py
+++ lib_mod.py
@@ -6,10 +6,15 @@ def mult(n1,n2):
 def add(n1,n2):
     return n1 + n2
 
+def substract(n1,n2):
+    return n1 - n2
 
 def test_mult_3_4():
     assert(mult(3,4) == 12)
 
 def test_add_3_4():
     assert(add(3,4) ==7)
+
+def test_substract_3_4():
+    assert(substract(3,4) == -1)
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #main
-from lib_mod import mult, add
+from lib_mod import mult, add, substract
 
 if __name__ == '__main__': 
     print mult(5,12)
     print add(5,12)
+    print substract(5,12)
 
