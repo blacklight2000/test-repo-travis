e83f40915679e65177c530b8ded12b2ce1225ff0
blacklight2000 <vphuvan@yahoo.com>
2015-11-15 12:13:50 -0500

    first commit

---
+++ lib_mod.py
@@ -0,0 +1,15 @@
+# module lib_mod
+
+def mult(n1,n2):
+    return n1 * n2
+
+def add(n1,n2):
+    return n1 + n2
+
+
+def test_mult_3_4():
+    assert(mult(3,4) == 12)
+
+def test_add_3_4():
+    assert(add(3,4) ==7)
+
---
+++ main.py
@@ -0,0 +1,7 @@
+##main
+from lib_mod import mult, add
+
+if __name__ == '__main__': 
+    print mult(5,12)
+    print add(5,12)
+
